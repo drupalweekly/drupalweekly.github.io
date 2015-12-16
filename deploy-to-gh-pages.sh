4f92a55650cff0d7744b50e1bcea12f5f17041f3
Gabriel Neutzling <gabrielneutzling@gmail.com>
2015-12-16 12:22:04 -0200

    Tests with TravisCI.

---
+++ deploy-to-gh-pages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf public || exit 0;
+mkdir public
+
+cd public
+git init
+git config user.name "Travis-CI"
+git config user.email "gabrielneutzling@gmail.com"
+cp ../CNAME ./CNAME
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
