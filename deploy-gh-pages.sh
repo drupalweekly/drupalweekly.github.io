c087d00734c3237ad6e8f45ea341eb777555c1f0
Gabriel Neutzling <gabrielneutzling@gmail.com>
2015-12-16 15:47:16 -0200

    Deploy script.

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Basically from https://medium.com/philosophy-logic/publishing-gh-pages-with-travis-ci-53a8270e87db
+
+rm -rf out || exit 0;
+mkdir out;
+
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "gabrielneutzling@gmail.com"
+
+cp ../CNAME ./CNAME
+cp ../index.html index.html
+cp -rf ../assets assets
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
