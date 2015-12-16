a21c4c7a9959709d44fcb1d1f4d2dc92f3413635
Gabriel Neutzling <gabrielneutzling@gmail.com>
2015-12-16 15:51:17 -0200

    Updated deploy script.

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -5,10 +5,14 @@
 rm -rf out || exit 0;
 mkdir out;
 
+GH_REPO="@github.com/drupalweekly/drupalweekly.github.io.git"
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
 cd out
 git init
 git config user.name "Travis-CI"
 git config user.email "gabrielneutzling@gmail.com"
+
 
 cp ../CNAME ./CNAME
 cp ../index.html index.html
@@ -16,4 +20,4 @@ cp -rf ../assets assets
 
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet $FULL_REPO master:gh-pages
